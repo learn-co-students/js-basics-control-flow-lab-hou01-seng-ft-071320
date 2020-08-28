1b4621e7d6ff60ef0192b7677c360be29aa80458
JoeG21 <jcggomez@live.com>
2020-08-27 22:13:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  // Write your code here!  
+  let message 
+
+  if (number > 2500) {
+    message = 'No can do.'
+  }
+  else if (number > 2000) {
+    message = 'I will gladly take your thirty bucks.'
+  }
+  else if (number <= 400){
+    message = 'This one is on me!'
+  }
+
+  return message
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC"? "Ok, sounds good.": "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message
+
+  switch(tip) {
+    case 'generous':
+      return message = 'Thank you so much.'
+      break
+    case 'not as generous':
+      return message = 'Thank you.'
+      break
+    default:
+      return message = 'Bye.'
+  }
+
 }
